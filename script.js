4bc384e88013f34b0bbbb97826479606c2be3e36
Lizza Reyes <lizzareyes13@gmail.com>
2017-05-27 00:27:37 -0400

    key codes implemented

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-var girlObj = document.getElementById('girl').style;
-var xGirl = 0;
+// var girlObj = document.getElementById('girl').style;
+// var xGirl = 0;
+//
+// document.onkeydown = KeyDownMoveIt;
+//
+// function KeyDownMoveIt(e){
+//   if (e.keyCode == 39)
+//   xGirl = xGirl + 10;
+//
+//   girlObj.left = xGirl;
+// }
 
-document.onkeydown = KeyDownMoveIt;
 
-function KeyDownMoveIt(e){
-  if (e.keyCode == 39)
-  xGirl = xGirl + 10;
+var girl = document.getElementById('girl');
+var board = document.getElementById('board');
 
-  girlObj.left = xGirl;
+var charLeft = 0;
+var charUp = 0;
+
+function charMovement(e){
+  // alert(e.keyCode);
+// -------------------
+  // LEFT ARROW >>>
+  if(e.keyCode == 39){
+    charLeft+=2;
+    girl.style.left = charLeft + 'px';
+    if(charLeft >=600){
+      charLeft-=2;
+    }
+  }
+  // RIGHT ARROW <<<
+  if(e.keyCode == 37){
+    charLeft-=2;
+    girl.style.left = charLeft + 'px';
+    if(charLeft <=0){
+      charLeft +=2;
+    }
+  }
+  // TOP ARROW ^^^
+  if(e.keyCode == 38){
+    charUp-=2;
+    girl.style.top = charUp + 'px';
+    if(charUp >= 600){
+      charUp+=2;
+    }
+  }
+  // BOTTOM ARROW vvv
+  if(e.keyCode == 40){
+    charUp+=2;
+    girl.style.top = charUp +'px';
+    if(charUp >= 600);
+      charUp+=2;
+  }
 }
 
-
-// var girl = document.getElementById('girl');
-// var board = document.getElementById('board');
-//
-// function anim(e){
-//   alert(e.keyCode);
-// }
-//
-// document.onkeydown = anim;
+document.onkeydown = charMovement;
